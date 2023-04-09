c0c85e032b7035f63e1a65a813aed9a6cab7ba74
saumyatalwani <saumyaofficial007@gmail.com>
2023-04-09 11:35:48 +0530

    added circle nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import { BrowserRouter as Router, Route, Routes  } from "react-router-dom";
+import { HashRouter as Router, Route, Routes  } from "react-router-dom";
 import Home from './pages/home'
 
 function App(){
     return(
         <Router>
             <Routes>
+                <Route exact path="/" element={<Home/>}/>
                 <Route exact path="/electricfish" element={<Home/>}/>
             </Routes>
         </Router>
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,34 +1,59 @@
 import React from 'react'
-
-
+//import { Link } from 'react-router-dom'
+import { HashLink } from 'react-router-hash-link'
 
 export default function Homepage() {
   
     return (
       <div>
+
         <div className='h-[100vh] w-full'>
-          <h1 className='text-6xl px-10 py-10'>Electric Fish</h1>
+          <h1 className='text-6xl px-10 py-10 heading'>ELECTRIC<br/>FISHES</h1>
         </div>
-        <div className='h-[100vh] '>
-          <a className='text-4xl px-10 py-5 float-left' href="#hist">History</a>
-          <a className='text-4xl px-10 py-5 float-left clear-left' href="#where">Where?</a>
-          <a className='text-4xl px-10 py-5 float-left clear-left' href="#how">How?</a>
-          <a className='text-4xl px-10 py-5 float-left clear-left' href="#exp">Experiment</a>
+
+        <div className='h-[100vh] heading'>
+
+          <HashLink className='text-4xl px-10 py-5 float-left' to="#hist">
+            <div className='rounded-full border-2 border-black aspect-square grid place-items-center'>
+              <p className=''>History</p>
+            </div>
+          </HashLink>
+
+          <HashLink className='text-4xl px-10 py-5 float-left' to="#where">
+            <div className='rounded-full border-2 border-black aspect-square grid place-items-center'>
+              <p className=''>Where</p>
+            </div>
+          </HashLink>
+
+          <HashLink className='text-4xl px-10 py-5 float-left' to="#how">
+            <div className='rounded-full border-2 border-black aspect-square grid place-items-center'>
+              <p className=''>How</p>
+            </div>
+          </HashLink>
+
+          <HashLink className='text-4xl px-10 py-5 float-left' to="#exp">
+            <div className='rounded-full border-2 border-black aspect-square grid place-items-center'>
+              <p className=''>Experiment</p>
+            </div>
+          </HashLink>
+
+
           
         </div>
+
         <div className='h-[100vh]' id='hist'>
-          <h2 className='text-4xl px-10 py-10'>History</h2>
+          <h2 className='text-4xl px-10 py-10 heading'>History</h2>
           <p className='px-10'>timeline goes here</p>
         </div>
         <div className='h-[100vh]' id='where'>
-          <h2 className='text-4xl px-10 py-10'>Where?</h2>
+          <h2 className='text-4xl px-10 py-10 heading'>Where?</h2>
           <p className='px-10'>worldmap goes here</p>
         </div>
         <div className='h-[100vh]' id='how'>
-          <h2 className='text-4xl px-10 py-10'>How?</h2>
+          <h2 className='text-4xl px-10 py-10 heading'>How?</h2>
         </div>
         <div className='h-[100vh]' id='exp'>
-          <h2 className='text-4xl px-10 py-10'>Experiment</h2>
+          <h2 className='text-4xl px-10 py-10 heading'>Experiment</h2>
         </div>
         
       </div>
