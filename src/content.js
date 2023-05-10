736a9a722f801492d482e9ab5fdf02c40d5c4943
saumyatalwani <saumyaofficial007@gmail.com>
2023-05-10 16:06:35 +0530

    new content

--- src/content.js
+++ src/content.js
@@ -1,12 +1,14 @@
 /* landing */
 
-const landDg1 = "Some fish have a unique ability to generate electric fields, making them \"electrogenic,\" while others can detect electric fields, making them \"electroreceptive.\" Most electric fish possess both of these abilities. There are several fish species, such as skates, rays, sharks, and catfish, that are electroreceptive, but not electrogenic. As a result, they are not typically referred to as electric fish."
+const landDg1 = "Apteronotus albifrons, commonly known as the black ghost knifefish or the black ghost electric fish, is a species of electric knife fish that belongs to the Apteronotidae family. It is a solitary and a nocturnal species. It is primarily a carnivorous animal and feeds on insect larvae, and small crustaceans."
 
-const landDg2 = "Apteronotus albifrons, commonly known as the black ghost knifefish or the black ghost electric fish, is a species of electric knifefish that belongs to the Apteronotidae family."
+const landDg2 = "Did you know that just like humans have different senses, such as sight and hearing, the black ghost knifefish has an electric sense? Meet the black ghost knifefish, a fascinating creature with an electric sense! Unlike us, it generates electric fields to navigate and communicate with other fish, especially in the dark and murky waters it calls home. "
 
-const landDg3 = "Similar to how humans have different senses like sight and hearing, the black ghost knifefish has an electric sense. It can generate electric fields which it uses for navigation and communication with other fish. This specialized sense is well-developed and allows the fish to adapt in the dark and murky waters it inhabits."
+const landDg3 = "This extraordinary sense is well-developed and gives the knifefish an edge in adapting to its environment. "
 
-const landDg4 = "Apteronotus albifrons is a solitary and a nocturnal species. It is primarily carnivorous and feeds on insect larvae, and small crustaceans."
+const landDg4 = "Some fish can produce electric fields, which makes them \"electrogenic\", while others can detect electric fields, which makes them \"electroreceptive\"."
+
+const landDg5 = "Many electric fish have both of these abilities. However, some fish species like skates, rays, sharks, and catfish can only detect electric fields, but not produce them. As a result, they are not usually considered electric fish."
 
 /* where */
  
@@ -16,9 +18,13 @@ const list = ['Astroscopus','Electrophorus','Apteronotus','Gymnotus','Torpedo','
 
 /* how */
 
-const howTxt = "It has specialized electrocytes in its tail that produce weak electric fields, which it uses to navigate its surroundings, locate prey, and communicate with other fish. Electric fish have the unique ability to generate electric fields around them using a specialized structure called an electric organ. This organ is typically located in the tails of electric fish and is made up of modified muscle or nerve cells that are capable of generating electric fields. The fields are a result of the Electric Organ Discharge (EOD), which is produced by the electric organ, as the name implies."
+const howTxt = "The Black Ghost Knife fish is quite the electro-sensation! It boasts specialised electrocytes in its tail that generates weak electric fields to navigate its surroundings, detect prey, and communicate with other fish. "
 
-/* EOD */
+const eodSbhd1="Electric Organ Discharge"
+
+const eod_P1="The electric fields are generated using a specialised electric organ in the tails of these fishes, made up of modified muscle or nerve cells. This organ produces an Electric Organ Discharge (EOD), which creates their unique ability to sense and communicate with their environment. "
+
+/* EOD - old
 
 const eodP1 = "Electric fish have the unique ability to generate electric fields around them using a specialized structure called an electric organ. This organ is typically located in the tails of electric fish and is made up of modified muscle or nerve cells that are capable of generating electric fields. The fields are a result of the Electric Organ Discharge (EOD), which is produced by the electric organ, as the name implies."
 
@@ -28,16 +34,19 @@ const eodP3 = "Weakly electric fish generate an EOD too weak to stun prey, but a
 
 const eodP4 = "that is typically less than one volt in amplitude."
 
-const eodSbhd = "Wave vs Pulse"
+*/
 
-const eodwp_p1 = "Some fish, like Apteronotus, Eigenmannia, and Gymnarchus, produce electric signals that are smooth and wavy, similar to the shape of a sine wave. These signals are continuous and consistent, and are known as \"wave-type\" electric organ discharges (EODs)."
+const eodSbhd2 = "Different Kinds of EODs: Wave vs Pulse"
 
-const eodwp_p2 = "Some fish species, like Gnathonemus, Gymnotus, and Raja, have a unique way of producing their electric organ discharge (EOD) waveform. Instead of a continuous waveform, their EOD consists of short pulses with longer gaps in between. This is referred to as a \"pulse-type\" EOD, as the electric discharges occur in brief bursts separated by longer pauses."
+const eodwp_p1 = "Apteronotus and other fish species like Eigenmannia, and Gymnarchus, produce electric signals that are smooth and wavy, similar to the shape of a sine wave. These signals are continuous and consistent, and are known as \"wave-type\" electric organ discharges (EODs)."
+
+const eodwp_p2 = "Instead of a continuous waveform, the EOD of Gnathonemus, Gymnotus, and Raja consists of short pulses with longer gaps in between. This is referred to as a \"pulse-type\" EOD, as the electric discharges occur in brief bursts separated by longer pauses."
 
 const eodwp_p3= "The figure above illustrates the different types of EOD waveforms observed in different species."
 
-const eodwp_p4 = "Apteronotus have a wave-type EOD, with a high discharge frequency (about 1 kHz) and a weak electric field (a few mV near the fish)."
+const eodwp_p4 = ""
 
-const content1 =['History','two lines worth of content goes here']
+const itb_sbhd="Foraging Behaviour: Is it Random?"
 
-module.exports={landDg1,landDg2,landDg3,landDg4,descText,list,howTxt,eodP1,eodP2,eodP3,eodP4,eodSbhd,eodwp_p1,eodwp_p2,eodwp_p3,eodwp_p4,content1}
+
+module.exports={landDg1,landDg2,landDg3,landDg4,landDg5,descText,list,howTxt,eodSbhd1,eod_P1,eodSbhd2,eodwp_p1,eodwp_p2,eodwp_p3,eodwp_p4,itb_sbhd}
--- src/pages/home.js
+++ src/pages/home.js
@@ -31,62 +31,72 @@ export default function Homepage() {
     return (
       <div>
 
-        <div className='h-[100vh] p-10 w-full'>
-          <div>
+        <div className='h-[100vh] p-10 w-full bg-fish '>
+          <div className='pb-[125px]'>
             <div onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
               <h1 className='text-6xl heading'>ELECTRIC<br/>FISH</h1>
-              <h1 className='italic text-3xl pb-5'>(Apteronotus albifrons)</h1>
+              <h1 className='italic text-3xl pb-5'>(Black Ghost Knife Fish)</h1>
             </div>
             {isHovering && <HoverDlg/>}
           </div>
-          
-          <p className='pb-5'>{data.landDg2}</p>
-          <p className='pb-5'>{data.landDg3}</p>
-          <p className='pb-5'>{data.landDg4}</p>
-        </div>
-      
-        <div className='h-[100vh] heading'>
-
-          <HashLink className='px-10 py-5 float-left' to="#hist">
-            <div className='p-5 lb1 text-4xl hover:bg-black hover:text-sm hover:text-white hover:opacity-60 w-[250px] rounded-full border-2 border-black aspect-square grid place-items-center'>
-            </div>
-          </HashLink>
-
-          <HashLink className='px-10 py-5 float-left' to="#where">
-            <div className='p-5 lb2 text-4xl hover:bg-black hover:text-sm hover:text-white hover:opacity-60 w-[250px] rounded-full border-2 border-black aspect-square grid place-items-center'> 
-            </div>
-          </HashLink>
-
-          <HashLink className='px-10 py-5 float-left' to="#how">
-            <div className='p-5 lb3 text-4xl hover:bg-black hover:text-sm hover:text-white hover:opacity-60 w-[250px] rounded-full border-2 border-black aspect-square grid place-items-center'>
-            </div>
-          </HashLink>
-
-          
-
-          <HashLink className='px-10 py-5 float-left' to="#exp">
-            <div className='p-5 lb4 text-2xl hover:bg-black hover:text-sm hover:text-white hover:opacity-60 w-[250px] rounded-full border-2 border-black aspect-square grid place-items-center'>
-            </div>
-          </HashLink>
-
-
+          <div>
+            <p className='pb-5'>{data.landDg2}</p>
+            <p className='pb-5'>{data.landDg3}</p>
+            <p className='pb-5'>{data.landDg4}</p>
+            <p className='pb-5'>{data.landDg5}</p>
+          </div>
           
         </div>
+        <h1 className='px-10 heading w-screen text-center text-4xl'> EXPLORE </h1>
+        <div className='grid h-[100vh] heading bg-fish place-items-center'>
 
-        <div className='relative h-screen py-10' id='hist'>
-          <h2 className='text-4xl heading px-10'>History</h2>
-          <iframe title="timeline" src='https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=1gdrUPByUq0VuKsgNHNihEd1zbXTFQzmz4uOOePT38_w&font=Default&lang=en&initial_zoom=2&height=650' width='100%' height='650' webkitallowfullscreen mozallowfullscreen allowfullscreen frameborder='0'></iframe>
-          <div className='px-10 absolute bottom-5'>
+          
+          <div>
+          <HashLink className='px-10 float-left' to="#hist">
+            <div className='p-5 lb1 text-4xl text-center hover:bg-black hover:text-xl hover:text-white hover:opacity-60 w-[250px] rounded-full border-2 border-black aspect-square grid place-items-center'>
+            </div>
+          </HashLink>
+
+          <HashLink className='px-10 float-left' to="#where">
+            <div className='p-5 lb2 text-4xl text-center hover:bg-black hover:text-sm hover:text-white hover:opacity-60 w-[250px] rounded-full border-2 border-black aspect-square grid place-items-center'> 
+            </div>
+          </HashLink>
+
+          <HashLink className='px-10 float-left' to="#how">
+            <div className='p-5 lb3 text-2xl text-center hover:bg-black hover:text-sm hover:text-white hover:opacity-60 w-[250px] rounded-full border-2 border-black aspect-square grid place-items-center'>
+            </div>
+          </HashLink>
+
+          <HashLink className='px-10 float-left' to="#exp">
+            <div className='p-5 lb4 text-4xl text-center hover:bg-black hover:text-xl hover:text-white hover:opacity-60 w-[250px] rounded-full border-2 border-black aspect-square grid place-items-center'>
+            </div>
+          </HashLink>
+          </div>
+          {
+          /*
+          <HashLink className='p-10'>
+            <div className='p-5 text-4xl text-center hover:bg-black hover:text-white hover:opacity-60'>
+            <p>Start</p>
+            </div>
+          </HashLink>
+          */}
+
+        </div>
+
+        <div className='relative h-screen py-10 bg-fish' id='hist'>
+          <h2 className='text-4xl heading px-10 pb-10'>Depths of Time</h2>
+          <iframe className='px-10 relative z-10' title="timeline" src='https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=1gdrUPByUq0VuKsgNHNihEd1zbXTFQzmz4uOOePT38_w&font=Default&lang=en&initial_zoom=2&height=650' width='100%' height='650' webkitallowfullscreen mozallowfullscreen allowfullscreen frameborder='0'></iframe>
+          <div className='px-10 absolute bottom-5 grid w-screen place-items-center z-10'>
             <HashLink to="#where">
               <img className="aspect-square h-[25px]" src="https://upload.wikimedia.org/wikipedia/commons/f/f2/Arrow_bottom_svg.svg" alt="down arrow"/>
             </HashLink>
           </div>
         </div>
 
-        <div className='relative p-10 h-screen' id='where'>
-          <h2 className='text-4xl heading'>Where?</h2>
-          <p className='py-5'>*worldmap goes here*</p>
-          <p className='pb-5'>{data.descText}</p>
+        <div className='relative p-10 h-screen bg-fish' id='where'>
+          <h2 className='text-4xl heading'>Where to find them?</h2>
+          <p className='py-5'>*to be added soon*</p>
+          {/*<p className='pb-5'>{data.descText}</p>
           <ul>
             {
               data.list.map(x=>{
@@ -94,39 +104,60 @@ export default function Homepage() {
               })
             }
           </ul>
-          <div className='absolute bottom-5'>
-            <HashLink to="#how">
+          */
+          }
+          <div className='absolute bottom-5 grid place-items-center w-screen'>
+            <HashLink to="#how1">
               <img className="aspect-square h-[25px]" src="https://upload.wikimedia.org/wikipedia/commons/f/f2/Arrow_bottom_svg.svg" alt="down arrow"/>
             </HashLink>
           </div>
         </div>
 
-        <div className='relative p-10 h-screen' id='how'>
-          <h2 className='text-4xl heading pb-5'>How?</h2>
-          <p>{data.howTxt}</p>
-          <div className='absolute bottom-5'>
+        <div className='relative p-10 h-screen bg-fish' id='how1'>
+          <h2 className='text-4xl heading pb-5'>What makes them electric?</h2>
+          <p className='pb-5'>{data.howTxt}</p>
+          <p className='py-5 font-bold text-4xl'>{data.eodSbhd1}</p>
+          <p className='pb-5'>{data.eod_P1}</p>
+          <div className='grid w-screen place-items-center absolute bottom-5'>
+            <HashLink to="#how2">
+              <img className="aspect-square h-[25px]" src="https://upload.wikimedia.org/wikipedia/commons/f/f2/Arrow_bottom_svg.svg" alt="down arrow"/>
+            </HashLink>
+          </div>
+        </div>
+
+        
+
+        <div className='relative p-10 h-screen bg-fish' id='how2'>
+
+        <h2 className='text-4xl heading pb-5'>What makes them electric?</h2>
+        <p className='py-5 font-bold text-4xl'>{data.eodSbhd2}</p>
+        <div className='grid place-items-center'>
+        <p className='pb-5'>{data.eodwp_p1}</p>
+          <p className='pb-5'>{data.eodwp_p2}</p>
+          <div className='place-items-center grid'>
+            <img src={diagram} alt='wave fish v/s pulse fish' className='py-10'></img>
+          </div>
+          <caption className='text-center w-[50vw]'>{data.eodwp_p3}</caption>
+          <p className='pb-5'>{data.eodwp_p4}</p>
+          <div className='absolute bottom-5 pt-5'>
             <HashLink to="#exp">
               <img className="aspect-square h-[25px]" src="https://upload.wikimedia.org/wikipedia/commons/f/f2/Arrow_bottom_svg.svg" alt="down arrow"/>
             </HashLink>
           </div>
         </div>
+          
+        </div>
 
-        <div className='h-screen p-10' id='exp'>
-          <h2 className='text-4xl pb-10 heading'>Experiment</h2>
+        <div className='h-screen p-10 bg-fish' id='exp'>
+          <h2 className='text-4xl pb-10 heading'>In the Lab</h2>
+          <p className='py-5 font-bold text-4xl'>{data.itb_sbhd}</p>
+          {/*
           <p className='pb-5'>{data.eodP1}</p>
           <p className='pb-5'>{data.eodP2}</p>
           <p className='pb-5'>{data.eodP3}</p>
           <p className='pb-5 font-bold'>{data.eodP4}</p>
-          <p className='py-5 font-bold text-4xl'>{data.eodSbhd}</p>
-          <p className='pb-5'>{data.eodwp_p1}</p>
-          <p className='pb-5'>{data.eodwp_p2}</p>
-          <div className='place-items-center grid'>
-            <img src={diagram} alt='wave fish v/s pulse fish' className='py-10'></img>
-          </div>
-          <p className='pb-5'>{data.eodwp_p3}</p>
-          <p className='pb-5'>{data.eodwp_p4}</p>
+          */}
         </div>
-        
       </div>
     )
   }
